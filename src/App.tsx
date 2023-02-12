0301555f59693be057bbbd50fb1f475e496a2f0f
Murilo Dutra Alves <malves@travelpassgroup.com>
2023-02-12 19:02:45 -0300

    Updating App component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useLoadScript } from '@react-google-maps/api';
+import {Map} from "./components/Map"
+ 
+function App() {
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY!,
+    libraries: ["places"]
+  })
+
+  if(!isLoaded) return <div>Loading...</div>
+  return (
+    <Map/>
+  );
+}
+
+export default App;
